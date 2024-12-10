45f0ddeb0bd4e4ad06bfe17df79a90175c625f93
Dionis <e2396498@cmaisonneuve.qc.ca>
2024-12-10 12:45:33 -0500

    Ajout de la base de la structure CSS et création du composant d'en-tête

---
+++ resources/views/bottle/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title', 'Liste des Bouteiilles')
+@section('content')
+
+<x-header 
+    image="{{ asset('img/header/header.jpg') }}" 
+    title="Découvrez notre collection" 
+    subtitle="C'est ce dont nous sommes fiers" 
+/>
+
+@endsection
