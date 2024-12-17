52b9e2ca0e07a75f139220b51096608c6145c47f
Dionis <e2396498@cmaisonneuve.qc.ca>
2024-12-17 00:23:31 -0500

    ajouter des donees pour extraire

--- app/Models/Bouteille.php
+++ app/Models/Bouteille.php
@@ -20,7 +20,16 @@ class Bouteille extends Model
         'degree_alcohol',
         'color',
         'size',
-
+        'description',
+        'designation_of_origin',
+        'classification',
+        'sugar_content',
+        'particularity',
+        'promoting_agent',
+        'producer',
+        'grape_variety',
+        'vintage_tasted',
+        'aromas',
     ];
 }
 
